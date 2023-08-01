73b02342126236b8c91127f25b4965a7c1f92343
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-08-01 12:05:49 +0200

    deleted routes for panier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Panier from "./pages/Panier";
+
 import SignUp from "./pages/SignUp";
 import Admin from "./pages/Admin";
 import Article from "./pages/article";
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="panier" element={<Panier />} />
+        
         <Route path="signup" element={<SignUp />} />
         <Route path="article/:id" element={<Article />} />
         <Route
