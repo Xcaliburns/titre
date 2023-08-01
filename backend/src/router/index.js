68dfd2efb3091910dafdcb96070adac0c3fed704
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-08-01 11:50:56 +0200

    erased unused files drom backend

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const productRoutes = require("./product.routes");
-const movieRoutes = require("./movie.routes.js");
+
 const userRoutes = require ("./user.routes.js")
-const cartRoutes=require("./cart.routes");
+
 const authRoutes = require("./auth.routes.js");
-const cartproductRoutes=require("./cartproduct.routes");
+
 const commentRoutes=require("./comment.routes");
 const authorization = require("../middleware/auth.js");
 
@@ -13,10 +13,7 @@ const router = express.Router();
 
  // les midlewares placés dans ce fichier seront global pour toutes les routes/*
 router.use("/product",productRoutes);
-router.use("/movie",authorization, movieRoutes);// toutes les route movies sont du coup soumises a authorization
 router.use("/user",userRoutes);
-router.use("/cart",authorization,cartRoutes);
-router.use("/cartproduct",authorization,cartproductRoutes);
 router.use("/comment",commentRoutes);
 router.use(authRoutes);
 
