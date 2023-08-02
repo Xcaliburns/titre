382a328283f939fc07d8139e9c98d5536555c980
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-08-02 12:33:43 +0200

    db updated

--- backend/database.sql
+++ backend/database.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comment` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `text` mediumtext NOT NULL,
   `user_id` int NOT NULL,
   `product_id` int NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE `comment` (
   KEY `fk_comment_product1_idx` (`product_id`),
   CONSTRAINT `fk_comment_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
   CONSTRAINT `fk_comment_user2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,6 +43,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (37,'on ne voit rien\n',2,16);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,13 +56,17 @@ DROP TABLE IF EXISTS `product`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `short_description` tinytext NOT NULL,
   `description` mediumtext NOT NULL,
   `price` float NOT NULL,
   `title` varchar(255) NOT NULL,
   `photo` varchar(500) NOT NULL,
+  `studio` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `release` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `idgame_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +75,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (16,'resume','description',5,'titre','photo','studio','genre','release');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,9 +91,10 @@ CREATE TABLE `user` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   UNIQUE KEY `iduser_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,6 +103,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (2,'david','david.abruzzo@sfr.fr','$argon2id$v=19$m=65536,t=5,p=1$7Ipo0NKn5cW5B0oGW74PXQ$37NJDutbDfZH5AXnC67S+6pyxlKhylg82OqQWr2jswY','admin'),(3,'toto','toto@toto.fr','$argon2id$v=19$m=65536,t=5,p=1$b4Ya3rGGR3nq1QtxrIIbbg$lhRFpuRP3Nx8l39Rd+5ngyEW3f4V8eWXzXDiDlIT4hE','user'),(4,'titi','titi@titi.fr','$argon2id$v=19$m=65536,t=5,p=1$JqKRcBum70ur+GrKuX2/aQ$eCT4/4/jKCLUNIwstUMdQaLts4EUg4Ob3J41x32r58w','`user`');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -108,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-14  8:25:14
+-- Dump completed on 2023-08-02 12:30:43
