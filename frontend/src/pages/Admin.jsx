cb41ebddcedab77c9b705f0614d5cd4889105d5a
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-08-02 12:23:50 +0200

    database and admin dashboard updated

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,17 +1,19 @@
-
 import { useState, useEffect } from "react";
 import CallApi from "../services/CallApi";
 import Navbar from "../components/Navbar";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function Admin() {
+  const [shortDescription, setShortDescription] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState([]);
   const [photo, setPhoto] = useState("");
   const [title, setTitle] = useState("");
+  const [studio, setStudio] = useState("");
+  const [genre, setGenre] = useState("");
+  const [release, setRelease] = useState("");
   const [productData, setProductData] = useState([]);
 
-  
   const [productId, setProductId] = useState("");
   const [deleteId, setDeleteId] = useState("");
 
@@ -21,22 +23,39 @@ function Admin() {
       .catch((err) => console.error(err));
   }, [productId]);
 
-  const handleSubmit = (err) => {
+  const handleSubmit = () => {
     setProductId(productId);
 
-    if (description && price && photo && title) {
+    if (
+      shortDescription &&
+      description &&
+      price &&
+      photo &&
+      title &&
+      studio &&
+      genre &&
+      release
+    ) {
       CallApi.post("api/product", {
+        shortDescription,
         description,
         price,
         photo,
         title,
+        studio,
+        genre,
+        release,
       })
         .then(() => {
+          setShortDescription("");
           setDescription("");
-          setPrice([])
+          setPrice([]);
           setPhoto("");
           setTitle("");
           setProductId("");
+          setStudio("");
+          setGenre("");
+          setRelease("");
 
           toast.success("🦄 article ajouté avec succès!", {
             position: "top-center",
@@ -56,20 +75,31 @@ function Admin() {
     }
   };
 
-  const handleUpdate = (e) => {
-    e.preventDefault();
-    if (description && price && photo && title)
+  const handleUpdate = (err) => {
+    err.preventDefault();
+    if (
+      shortDescription &&
+      description &&
+      price &&
+      photo &&
+      title &&
+      studio &&
+      genre &&
+      release
+    )
       CallApi.put(`/api/product/${productId}`, {
+        shortDescription,
         description,
-
         price,
-
         photo,
         title,
+        studio,
+        genre,
+        release,
       })
         .then(() => {
-          // Affiche le toast de réussite
-          toast.success("Produit mis à jour avec succès !");
+          
+          toast.success("Article mis à jour avec succès !");
         })
         .catch((err) => console.log(err.response.data));
     else {
@@ -77,29 +107,40 @@ function Admin() {
     }
   };
 
-  const handleDelete = (err) => {
-    //err.preventDefault();
-    if (deleteId)
+  const handleDelete = (e) => {
+    e.preventDefault();
+    if (deleteId) {
       CallApi.delete(`/api/product/${deleteId}`)
-        .then(() => {})
+        .then(() => {
+          
+          toast.success("Produit supprimé avec succès !", {
+            position: "center",
+            autoClose: 5000,
+            hideProgressBar: false,
+          });
+        })
         .catch((err) => console.log(err.response.data));
-    else {
-      // Affiche le toast de réussite
-      toast.success("Produit supprimé avec succès !", {
+    } else {
+      
+      toast.error("Erreur lors de la suppression !", {
         position: "center",
         autoClose: 5000,
         hideProgressBar: false,
       });
     }
   };
+  
 
   const handleProductChange = (e) => {
     setProductId(e.target.value);
   };
 
-  const handleDeleteId = (err) => {
-    setDeleteId(err.target.value);
+  const handleDeleteId = (e) => {
+
+    setDeleteId(e.target.value);
   };
+
+  console.log(`${shortDescription}, ${description}, ${price}, ${photo}, ${title}, ${studio}, ${genre}, ${release}`);
 
   return (
     <div className="flex flex-col  items-center text-xl bg-gray-500 min-h-full">
@@ -107,28 +148,56 @@ function Admin() {
 
       <div className="flex flex-col lg:flex-row">
         <div className="w-full px-6 py-4 m-5 mt-10 overflow-hidden bg-gray-800 shadow-xl border-solid sm:max-w-md sm:rounded-lg">
+          
           <form className="bg-gray-800 " onSubmit={handleSubmit}>
-            <div className="mt-10 min-h-full">
-              <label
-                htmlFor="text"
-                className="block text-sm font-medium text-gray-400 "
-              >
-                description
-              </label>
+
+            
+              <div className="mt-4">
+                <label
+                  htmlFor="text"
+                  className="block text-sm font-medium text-gray-400 "
+                >
+                  resumé
+                </label>
+              </div>
               <div className="flex flex-col items-start">
                 <input
-                  onChange={(e) => setDescription(e.target.value)}
-                  value={description}
+                  onChange={(e) => setShortDescription(e.target.value)}
+                  value={shortDescription}
                   type="text"
-                  name="description"
+                  name="shortDescription"
                   className="block w-2/3 rounded-md"
-                  id="description"
+                  id="shortDescription"
                   minLength="4"
                   maxLength="100"
                   size="80"
                 />
               </div>
-            </div>
+
+
+              <div className="mt-4">
+                <label
+                  htmlFor="text"
+                  className="block text-sm font-medium text-gray-400 "
+                >
+                  description
+                </label>
+                <div className="flex flex-col items-start">
+                  <input
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                    type="text"
+                    name="description"
+                    className="block w-2/3 rounded-md"
+                    id="description"
+                    minLength="4"
+                    maxLength="100"
+                    size="80"
+                  />
+                </div>
+              </div>
+
+           
             <div className="mt-4">
               <label
                 htmlFor="text"
@@ -141,12 +210,14 @@ function Admin() {
                   onChange={(e) => setPrice(e.target.value)}
                   value={price}
                   type="text"
-                  name="Id"
+                  name="price"
                   className=" block w-2/3 rounded-md"
-                  id="Id"
+                  id="price"
                 />
               </div>
             </div>
+
+
             <div className="mt-4">
               <label
                 htmlFor="text"
@@ -165,6 +236,8 @@ function Admin() {
                 />
               </div>
             </div>
+
+
             <div className="mt-4">
               <label
                 htmlFor="text"
@@ -179,10 +252,71 @@ function Admin() {
                   type="text"
                   name="title"
                   className=" block w-2/3 rounded-md"
-                  id="photo"
+                  id="title"
                 />
               </div>
             </div>
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 "
+              >
+                studio
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setStudio(e.target.value)}
+                  value={studio}
+                  type="text"
+                  name="studio"
+                  className=" block w-2/3 rounded-md"
+                  id="studio"
+                />
+              </div>              
+            </div>
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 "
+              >
+                genre
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setGenre(e.target.value)}
+                  value={genre}
+                  type="text"
+                  name="genre"
+                  className=" block w-2/3 rounded-md"
+                  id="genre"
+                />
+              </div>              
+            </div>
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 "
+              >
+                release
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setRelease(e.target.value)}
+                  value={release}
+                  type="text"
+                  name="release"
+                  className=" block w-2/3 rounded-md"
+                  id="release"
+                />
+              </div>
+            </div>
+            
             <button
               type="button"
               className="inline-flex items-center px-4 py-2 mt-4 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900 false hover:bg-green-500"
@@ -193,9 +327,35 @@ function Admin() {
             <ToastContainer />
           </form>
         </div>
+
+
+
         <div className="px-6 py-4 mt-10 m-5 overflow-hidden bg-gray-800 shadow-xl border-solid sm:max-w-md sm:rounded-lg w-full min-h-800">
           <form className="bg-gray-800 " onSubmit={handleUpdate}>
             <div className="bg">
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 undefined"
+              >
+                résumé
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setShortDescription(e.target.value)}
+                  value={shortDescription}
+                  type="text"
+                  name="shortDescription"
+                  className="block w-2/3 rounded-md"
+                  id="shortDescription"
+                />
+              </div>
+              </div>
+
+
+            <div className="mt-4">
               <label
                 htmlFor="text"
                 className="block text-sm font-medium text-gray-400 undefined"
@@ -212,7 +372,10 @@ function Admin() {
                   id="description"
                 />
               </div>
+              </div>
             </div>
+
+
             <div className="mt-4">
               <label
                 htmlFor="text"
@@ -231,6 +394,8 @@ function Admin() {
                 />
               </div>
             </div>
+
+
             <div className="mt-4">
               <label
                 htmlFor="text"
@@ -250,6 +415,7 @@ function Admin() {
               </div>
             </div>
 
+
             <div className="mt-4">
               <label
                 htmlFor="text"
@@ -268,6 +434,67 @@ function Admin() {
                 />
               </div>
             </div>
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 "
+              >
+                studio
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setStudio(e.target.value)}
+                  value={studio}
+                  type="text"
+                  name="text"
+                  className=" block w-2/3 rounded-md"
+                  id="price"
+                />
+              </div>
+            </div>
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 "
+              >
+                genre
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setGenre(e.target.value)}
+                  value={genre}
+                  type="text"
+                  name="genre"
+                  className=" block w-2/3 rounded-md"
+                  id="genre"
+                />
+              </div>
+            </div>
+
+
+            <div className="mt-4">
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-400 "
+              >
+                release
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  onChange={(e) => setRelease(e.target.value)}
+                  value={release}
+                  type="text"
+                  name="release"
+                  className=" block w-2/3 rounded-md"
+                  id="release"
+                />
+              </div>
+            </div>
+
 
             <div className="mt-4">
               <label
@@ -304,6 +531,8 @@ function Admin() {
         </div>
         <div className="w-full px-6 py-4 lg:min-w-500 m-5 mt-10 overflow-hidden bg-gray-800 shadow-xl border-solid sm:max-w-md sm:rounded-lg">
           <form className="bg-gray-800 " onSubmit={handleUpdate}>
+
+
             <div className="mt-4">
               <label
                 htmlFor="titre"
@@ -334,6 +563,7 @@ function Admin() {
             >
               Effacer
             </button>
+            <ToastContainer />
           </form>
         </div>
       </div>
