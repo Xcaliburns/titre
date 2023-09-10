7c823da0166fadf0a653019e8708431d80f68d4d
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-09-10 16:09:20 +0200

    last fix

--- frontend/src/Components/productCard.jsx
+++ frontend/src/Components/productCard.jsx
@@ -20,12 +20,12 @@ function ProductCard({ product }) {
         src={product.photo}
         alt={product.title}
       />
-      <div className="flex flex-col items-center w-max-150 w-min-150     ">
+      <div className="flex flex-col items-center w-max-150 w-min-150 ">
         <div className="  w-48 h-6 text-center truncate">{product.title}</div>
         <div className="  w-48 h-54 text-center truncate">
           {product.short_description}
         </div>
-        <button className=" flex-shrink-0 inline-flex items-center px-4 py-2 mt-1 ml-4 text-xs font-semibold tracking-widest text-gray-100 text-lg uppercase transition duration-150 ease-in-out bg-gray-600 border border-transparent rounded-md active:bg-gray-600 false hover:bg-blue-500">
+        <button className=" flex-shrink-0 inline-flex items-center px-4 py-2 mt-1 ml-4 text-xs font-semibold tracking-widest text-gray-100 text-lg uppercase transition duration-150 ease-in-out bg-gray-600 border border-transparent rounded-md active:bg-gray-600  hover:bg-blue-500">
           lire l'article
         </button>
       </div>
