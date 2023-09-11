f9e3fa5f51bf5d0a27e88926362f7b2409ae61a9
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-09-11 13:46:45 +0200

    fix

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -2,14 +2,13 @@ const { decodeJWT } = require("../helper/jwt.helper.js");
 
 const authorization = async (req, res, next) => {
   try {
-    const token = req.cookies.auth_token;
+    const token = req.cookies?.auth_token;
 
     if (!token) throw new Error();
 
     const data = decodeJWT(token);
 
-    req.userID = data.id;
-    req.userName = data.name;
+    req.user = data;
 
     return next();
   } catch (e) {
